6994b59f00b99ebaa4fddf4f5afca9f926af1ca5
Mirjana Babic <mirjana_babic@outlook.com>
2022-11-04 15:13:48 +0100

    Some change to first solution without functions

--- project_pig_game/main.py
+++ project_pig_game/main.py
@@ -5,21 +5,27 @@ scores = [0, 0]
 current_score = 0
 
 max_score = int(input("What is the maximum score for playing that you want? "))
+print()
 print(f"Max score is {max_score}")
 
 while True:
-    print("Total score", scores[active_player])
+    print('----------')
+    print()
+    print("Total score is", scores[active_player])
     dice = random.randint(1,6)
     print("You got ", dice)
 
     if dice != 1:
         current_score += dice
         print(f"Current score is {current_score}")
-        print('-----------')
         print(f"Current player is {active_player}")
+        print()
+        print('-----------')
 
         if current_score >= max_score:
+            print('***********')
             print(f"Player {active_player} won")
+            print('***********')
             break
 
         answer = input("Do you want to save your score and let second player to play(press y if you want that).")
@@ -27,7 +33,9 @@ while True:
             scores[active_player] += current_score
             current_score = 0
             if scores[active_player] >= max_score:
+                print('***********')
                 print(f"Player {active_player} won")
+                print('***********')
                 break
 
             # active_player = 1 if active_player == 0 else 0
@@ -35,17 +43,29 @@ while True:
 
             if active_player == 0:
                 active_player = 1
+                print()
+                print("PLAYERS ARE CHANGED")
+                print('Now is another turn for another player')
                 print(f"Active player now is {active_player}")
-                current_score = scores[active_player]
+                current_score = 0
+                print()
             else:
                 active_player = 0
+                print('Now is another turn for another player')
                 print(f'Active player now is {active_player}')
-                current_score = scores[active_player]
+                current_score = 0
+                print()
+        print()
+        print(f"Player {active_player} is still playing.")
     else:
+        print()
         print("Now is turn for another player, you got 1")
+        print("PLAYERS ARE CHANGED")
         current_score = 0
         if active_player == 0:
             active_player = 1
         else:
             active_player = 0
+        print()
         print(f'Active player now is {active_player}')
+        print()
