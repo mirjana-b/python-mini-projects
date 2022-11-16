5c7134056c0da8fe97120233b451eee730b1e0ce
Mirjana Babic <mirjana_babic@outlook.com>
2022-11-16 18:30:30 +0100

    Clean up the code

--- quiz_game/game.py
+++ quiz_game/game.py
@@ -1,14 +1,19 @@
 import os
 
 
-def playing_the_game(questions):
-    os.system("cls")
+def print_intro():
     print("Welcome to the quiz game. Some instruction before playing:")
     print("1. The game is made out of five questions.")
     print("2. You will answer only one question at a time.")
     print("3. You will get a list of potential answers to select from.")
     print("4. Good luck and please enjoy your game! 😃")
     print()
+
+
+def run_game_logic(questions):
+    os.system("cls")
+    print_intro()
+
     answer = input("If you want to continue the game press c key(continue): ")
     score = 0
     questions_answered_correctly = []
@@ -16,21 +21,21 @@ def playing_the_game(questions):
     if answer == 'c':
         os.system("cls")
 
-        for i in range(0, 5):
-            print(f"Your question number {i+1} is: {questions[i].question}")
+        for i, question in enumerate(questions):
+            print(f"Your question number {i+1} is: {question.question}")
             print(
-                f"This is the list of potential answers: {questions[i].answers}")
+                f"This is the list of potential answers: {question.answers}")
             print()
             selected_answer = int(input(
                 "Select your answer(write the index of your answer, index starts from 0): "))
 
-            if selected_answer == questions[i].correctIndex:
+            if selected_answer == question.correct_index:
                 score += 1
-                questions_answered_correctly.append(questions[i].question)
+                questions_answered_correctly.append(question.question)
 
             os.system("cls")
 
-            if i == 4:
+            if i == len(questions) - 1:
                 print("This was your last question")
 
         print(f"Your score is {score}.")
--- quiz_game/main.py
+++ quiz_game/main.py
@@ -1,52 +1,51 @@
 import json
 import random
 
-from game import playing_the_game
+from game import run_game_logic
+
+QUESTIONS_IN_QUIZ = 5
+QUESTIONS_FILE = "questions.json"
 
 
 class QuestionAndAnswer:
-    def __init__(self, question, answers, correctIndex):
+    """A class that represents a question and offered answers.
+    """
+
+    def __init__(self, question, answers, correct_index):
         self.question = question
         self.answers = answers
-        self.correctIndex = correctIndex
+        self.correct_index = correct_index
 
 
-def from_quiz_questions(question):
-    """This function generates object which belongs to the class QuestionAndAnswer
+def question_from_dictionary(question):
+    """Create object of class QuestionAndAnswer from a dictionary.
 
     Args:
-        question (dict): This function expects dictionary as an argument
+        question (dict): A dictionary from which to create an object.
 
     Returns:
-        object: object that belongs to the class QuestionAndAnswer
+        object: object of class QuestionAndAnswer
     """
     question_and_answer = QuestionAndAnswer(
         question['question'], question['answers'], question['correctIndex'])
     return question_and_answer
 
 
-def select_random_questions(questions):
-    random.shuffle(questions)
-    questions_for_quiz = []
-
-    for i in range(0, 5):
-        questions_for_quiz.append(questions[i])
-
-    return questions_for_quiz
+def select_random_questions(questions, questions_in_quiz):
+    return random.sample(questions, k=questions_in_quiz)
 
 
 def main():
-    with open("questions.json", "r", encoding="utf-8") as f:  # pylint: disable=invalid-name
+    with open(QUESTIONS_FILE, "r", encoding="utf-8") as f:  # pylint: disable=invalid-name
         quiz_questions = json.load(f)
     quiz_questions = quiz_questions['questions']
-    quiz_objects = []
+    quiz_objects = [question_from_dictionary(
+        question) for question in quiz_questions]
 
-    for question in quiz_questions:
-        quiz_objects.append(from_quiz_questions(question))
+    questions_for_quiz = select_random_questions(
+        quiz_objects, QUESTIONS_IN_QUIZ)
 
-    questions_for_quiz = select_random_questions(quiz_objects)
-
-    playing_the_game(questions_for_quiz)
+    run_game_logic(questions_for_quiz)
 
 
 if __name__ == "__main__":
