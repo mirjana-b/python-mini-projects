c48b69b139d52e667c9995ba097eb0f01f111655
Mirjana Babic <mirjana_babic@outlook.com>
2022-11-17 12:35:43 +0100

    Add print functions for printing potential answers and questions answered correctly

--- quiz_game/game.py
+++ quiz_game/game.py
@@ -6,8 +6,20 @@ def print_intro():
     print("1. The game is made out of five questions.")
     print("2. You will answer only one question at a time.")
     print("3. You will get a list of potential answers to select from.")
-    print("4. Good luck and please enjoy your game! 😃")
+    print("4. Good luck and please enjoy your game!")
     print()
+
+
+def print_potential_answers(list_of_answers):
+    print("This is the list of potential answers:")
+    for i in range(0, len(list_of_answers)):
+        print(f"{i+1}. {list_of_answers[i]}")
+
+
+def print_questions_answered_correctly(questions):
+    print("This is a list of questions that you answered correctly:")
+    for i in range(0, len(questions)):
+        print(f"{i+1}. {questions[i]}")
 
 
 def run_game_logic(questions):
@@ -23,13 +35,11 @@ def run_game_logic(questions):
 
         for i, question in enumerate(questions):
             print(f"Your question number {i+1} is: {question.question}")
-            print(
-                f"This is the list of potential answers: {question.answers}")
+            print_potential_answers(question.answers)
             print()
-            selected_answer = int(input(
-                "Select your answer(write the index of your answer, index starts from 0): "))
+            selected_answer = int(input("Select your answer: "))
 
-            if selected_answer == question.correct_index:
+            if selected_answer - 1 == question.correct_index:
                 score += 1
                 questions_answered_correctly.append(question.question)
 
@@ -39,8 +49,7 @@ def run_game_logic(questions):
                 print("This was your last question")
 
         print(f"Your score is {score}.")
-        print(
-            f"This is a list of questions that you answered correctly. {questions_answered_correctly}")
+        print_questions_answered_correctly(questions_answered_correctly)
 
     else:
         print("You selected not to play the game. Bye 🙋‍♀️")
