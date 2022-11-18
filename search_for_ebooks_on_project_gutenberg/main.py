1c06b0c292bb3f98065d8a85d5fe8a19d3b066a7
Mirjana Babic <mirjana_babic@outlook.com>
2022-11-18 14:28:40 +0100

    Implement check if the response code is 200(OK)

--- search_for_ebooks_on_project_gutenberg/main.py
+++ search_for_ebooks_on_project_gutenberg/main.py
@@ -27,18 +27,21 @@ def print_search_results(book_objects):
 
 def main():
     os.system("cls")
-    # TODO Check if the response code is 200 (OK). Also check if there is already
-    # a constant for that 200
-    # https://requests.readthedocs.io/en/latest/api/#status-code-lookup
     user_query = input(
         "Enter words to search author names and book titles on Project Gutenberg: ")
+
     response = requests.get(create_request_url(user_query),
-                            timeout=CONNECTION_TIMEOUT).text
-    json_object = json.loads(response)  # dict
-    results = json_object['results']  # list of dictionaries
-    # list of objects of class Book
-    books = [Book.from_dictionary(result) for result in results]
-    print_search_results(books)
+                            timeout=CONNECTION_TIMEOUT)
+
+    if response.status_code == requests.codes['ok']:
+        response_text = response.text
+        json_object = json.loads(response_text)  # dict
+        results = json_object['results']  # list of dictionaries
+        # list of objects of class Book
+        books = [Book.from_dictionary(result) for result in results]
+        print_search_results(books)
+    else:
+        print("Response status code wasn't OK!")
 
 
 if __name__ == "__main__":
