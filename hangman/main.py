593ad8c218d43d0780c92340acaedb7147a97eb7
Mirjana Babic <mirjana_babic@outlook.com>
2022-12-06 16:43:21 +0100

    Refactor the code

--- hangman/main.py
+++ hangman/main.py
@@ -69,10 +69,11 @@ def letter_check(letter_guess, country_guess, country, missed_letters):
     return country_guess, missed_letters
 
 
-def print_hangman(missed_letters, hangman):
+def print_hangman(missed_letters, hangman, country):
     os.system('cls')
     if len(missed_letters) >= len(hangman):
-        print(f"{Fore.RED}You are hanged!{Style.RESET_ALL}")
+        print(
+            f"{Fore.RED}You are hanged! The secret country was: {country}{Style.RESET_ALL}")
         print()
         print(f"{Fore.RED}{hangman[len(missed_letters)-1]}{Style.RESET_ALL}")
     else:
@@ -85,29 +86,30 @@ def main():
     country = select_country()
     country_guess = ['_']*len(country)
     missed_letters = ''
-    country_guess_new = ''
+    country_guess_result = ''
     os.system('cls')
     while playing:
         letter_guess = input("Enter your letter: ")
         country_guess, missed_letters = letter_check(
             letter_guess, country_guess, country, missed_letters)
-        country_guess_new = "".join(country_guess)
-        if country == country_guess_new:
+        country_guess_result = "".join(country_guess)
+        if country == country_guess_result:
             os.system('cls')
             print(
-                f"{Fore.GREEN}You have guessed the country, congrats! Secret country was {country_guess_new}.{Style.RESET_ALL}")
+                f"{Fore.GREEN}You have guessed the country, congrats!" +
+                f" Secret country was {country_guess_result}.{Style.RESET_ALL}")
             playing = False
             continue
         elif len(missed_letters) >= len(HANGMAN_PICS):
             playing = False
-            print_hangman(missed_letters, HANGMAN_PICS)
+            print_hangman(missed_letters, HANGMAN_PICS, country)
         elif len(missed_letters) == 0:
             os.system('cls')
-            print(f"Your progress so far: {country_guess_new}")
+            print(f"Your progress so far: {country_guess_result}")
         if 0 < len(missed_letters) < len(HANGMAN_PICS):
-            print_hangman(missed_letters, HANGMAN_PICS)
+            print_hangman(missed_letters, HANGMAN_PICS, country)
             print()
-            print(f"Your progress so far: {country_guess_new}")
+            print(f"Your progress so far: {country_guess_result}")
 
 
 if __name__ == "__main__":
