1bea001502206c26ce58f0eea51236619e737ef4
Mirjana Babic <mirjana_babic@outlook.com>
2023-01-12 14:28:50 +0100

    Implement choosing to list visible or hidden files.

--- ls_command/main.py
+++ ls_command/main.py
@@ -5,17 +5,19 @@ from pathlib import Path
 
 parser = argparse.ArgumentParser(description="List contents of a directory.")
 parser.add_argument('dir', nargs='?', default=os.getcwd(), type=Path, help="Directory path.")
+parser.add_argument('-a', '--all', action = "store_true" ,help="Show everything, including hidden items.")
+args = parser.parse_args()
+dir_path_abs = args.dir.resolve().absolute()
+show_all = args.all
 
-arg = parser.parse_args()
-dir_path_abs = arg.dir.resolve().absolute()
-
-# print (f"This is the absolute path to current working directory {dir_path_abs}")
-
-def listing_all_files(dir):
-    for f in dir.iterdir():
-        print(f.name)
+def listing_all_files(directory, show_hidden_files):
+    for f in directory.iterdir():
+        if show_hidden_files:
+            print(f.name)
+        else:
+            if f.name[0]!='.':
+                print (f.name)
 
 
 if __name__ == '__main__':
-    listing_all_files(dir_path_abs)
-
+    listing_all_files(dir_path_abs, show_all)
