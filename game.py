07db45d38bffcffd0b218f162bb07f7d18637a2b
Mirjana Babic <mirjana_babic@outlook.com>
2023-02-01 13:19:20 +0100

    Refactor the code

--- game.py
+++ game.py
@@ -2,17 +2,16 @@ import time
 import os
 import pygame
 
+
 class Game():
     def __init__(self):
         pygame.init()# pylint: disable=no-member
         self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)# pylint: disable=no-member
         self.background = pygame.image.load(os.path.join("./pictures", "space.png"))
         self.running = True
-        self.d_time, self.prev_time = 0, 0
 
     def game_loop(self):
         while self.running:
-            self.get_dt()
             self.get_events()
             self.update()
             self.render()
@@ -21,7 +20,6 @@ class Game():
         for event in pygame.event.get():
             if event.type == pygame.QUIT: # pylint: disable=no-member
                 self.running = False
-        #TODO implement more events after I decide what will be my game logic
 
     def update(self):
         pass
@@ -29,11 +27,6 @@ class Game():
     def render(self):
         self.screen.blit(self.background, (0, 0))
         pygame.display.flip()
-
-    def get_dt(self):
-        now = time.time()
-        self.d_time = now - self.prev_time
-        self.prev_time = now
 
 
 if __name__ == "__main__":
