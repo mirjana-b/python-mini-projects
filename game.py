626984d4798f5ce283f27378aa848e928b704ae2
Mirjana Babic <mirjana_babic@outlook.com>
2023-02-01 14:50:52 +0100

    Implement buttons play and exit

--- game.py
+++ game.py
@@ -1,7 +1,7 @@
 import time
 import os
 import pygame
-
+from button import Button
 
 class Game():
     def __init__(self):
@@ -9,6 +9,11 @@ class Game():
         self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)# pylint: disable=no-member
         self.background = pygame.image.load(os.path.join("./pictures", "space.png"))
         self.running = True
+        self.w, self.h = pygame.display.get_surface().get_size()
+        self.play_button_img = pygame.image.load(os.path.join("./pictures", "play_button.png"))
+        self.exit_button_img = pygame.image.load(os.path.join("./pictures", "exit_button.png"))
+        self.play_button = Button(self.w/2 - 275 - 10, self.h/2 - 70, self.play_button_img)
+        self.exit_button = Button(self.w/2 + 10, self.h/2 - 70, self.exit_button_img)
 
     def game_loop(self):
         while self.running:
@@ -26,6 +31,8 @@ class Game():
 
     def render(self):
         self.screen.blit(self.background, (0, 0))
+        self.play_button.draw(self.screen)
+        self.exit_button.draw(self.screen)
         pygame.display.flip()
 
 
