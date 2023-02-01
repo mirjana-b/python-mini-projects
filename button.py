3f47362a356a6c1aa4a5aca6316ba76921d935f9
Mirjana Babic <mirjana_babic@outlook.com>
2023-02-01 14:44:30 +0100

    Add button class

---
+++ button.py
@@ -0,0 +1,12 @@
+import os
+import pygame
+
+
+class Button:
+    def __init__(self, x, y, image):
+        self.image = image
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (x,y)
+
+    def draw(self, screen):
+        screen.blit(self.image, (self.rect.x, self.rect.y))
