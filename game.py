b42d0f92ca2902cc1659d2e5b3acbaabf26b29f5
Mirjana Babic <mirjana_babic@outlook.com>
2023-02-06 17:02:01 +0100

    Add planet positions and render them when playing

--- game.py
+++ game.py
@@ -43,7 +43,22 @@ class Game():
         self.initialize_planets()
 
     def initialize_planets(self):
-        width, height = pygame.display.get_surface().get_size()
+        andoria_pos_x = 680
+        andoria_pos_y = 380
+        ardana_pos_x = 466
+        ardana_pos_y = 199
+        elball_pos_x = 1031
+        elball_pos_y = 180
+        nibiru_pos_x = 160
+        nibiru_pos_y = 90
+        omegaIV_pos_x = 487
+        omegaIV_pos_y = 640
+        risa_pos_x = 1200
+        risa_pos_y = 500
+        vulcan_pos_x = 750
+        vulcan_pos_y = 50
+        wolf359_pos_x = 145
+        wolf359_pos_y = 510
 
         andoria_planet_img = pygame.image.load(
             os.path.join("./pictures", "andoria.png"))
@@ -54,7 +69,7 @@ class Game():
         nibiru_planet_img = pygame.image.load(
             os.path.join("./pictures", "nibiru.png"))
         omegaIV_planet_img = pygame.image.load(
-            os.path.join("./pictures", "risa.png"))
+            os.path.join("./pictures", "omegaIV.png"))
         risa_planet_img = pygame.image.load(
             os.path.join("./pictures", "risa.png"))
         vulcan_planet_img = pygame.image.load(
@@ -63,36 +78,36 @@ class Game():
             os.path.join("./pictures", "wolf359.png"))
 
         self.andoria_planet = Planet(
-            x=width/2,
-            y=height/2,
+            x=andoria_pos_x,
+            y=andoria_pos_y,
             image=andoria_planet_img)
         self.ardana_planet = Planet(
-            x=width/2,
-            y=height/2,
+            x=ardana_pos_x,
+            y=ardana_pos_y,
             image=ardana_planet_img)
         self.elball_planet = Planet(
-            x=width/2,
-            y=height/2,
+            x=elball_pos_x,
+            y=elball_pos_y,
             image=elball_planet_img)
         self.nibiru_planet = Planet(
-            x=width/2,
-            y=height/2,
+            x=nibiru_pos_x,
+            y=nibiru_pos_y,
             image=nibiru_planet_img)
         self.omegaIV_planet = Planet(
-            x=width/2,
-            y=height/2,
+            x=omegaIV_pos_x,
+            y=omegaIV_pos_y,
             image=omegaIV_planet_img)
         self.risa_planet = Planet(
-            x=width/2,
-            y=height/2,
+            x=risa_pos_x,
+            y=risa_pos_y,
             image=risa_planet_img)
         self.vulcan_planet = Planet(
-            x=width/2,
-            y=height/2,
+            x=vulcan_pos_x,
+            y=vulcan_pos_y,
             image=vulcan_planet_img)
         self.wolf359_planet = Planet(
-            x=width/2,
-            y=height/2,
+            x=wolf359_pos_x,
+            y=wolf359_pos_y,
             image=wolf359_planet_img)
 
     def game_loop(self):
@@ -123,6 +138,13 @@ class Game():
 
         if self.playing:
             self.andoria_planet.draw(self.screen)
+            self.ardana_planet.draw(self.screen)
+            self.elball_planet.draw(self.screen)
+            self.nibiru_planet.draw(self.screen)
+            self.omegaIV_planet.draw(self.screen)
+            self.risa_planet.draw(self.screen)
+            self.vulcan_planet.draw(self.screen)
+            self.wolf359_planet.draw(self.screen)
         else:
             self.play_button.draw(self.screen)
             self.exit_button.draw(self.screen)
