fff08024735f579415d7625603d39eff3598a904
Mirjana Babic <mirjana_babic@outlook.com>
2023-02-15 17:15:17 +0100

    Add planet names on hover

--- space_and_stuff/planet.py
+++ space_and_stuff/planet.py
@@ -2,10 +2,18 @@ import pygame
 
 
 class Planet:
-    def __init__(self, x, y, image):
+    def __init__(self, x, y, image, name):
         self.image = image
         self.rect = self.image.get_rect()
         self.rect.topleft = (x,y)
+
+        planet_name = name
+        font = pygame.font.SysFont(None, 40)
+        text_color = (255,255,255)
+
+        self.text_offset_x = -18
+        self.text_offset_y = 50
+        self.planet_name= font.render(planet_name, True, text_color)
         self.hover = False
 
 
@@ -20,8 +28,21 @@ class Planet:
         return self.hover
 
     def draw(self, screen):
-        if self.is_hovering():
-            new_image = pygame.transform.scale(self.image, (200, 200))
-            screen.blit(new_image, (self.rect.x, self.rect.y))
-        else:
+        if not self.is_hovering():
             screen.blit(self.image, (self.rect.x, self.rect.y))
+            return
+
+        new_image = pygame.transform.scale(self.image, (200, 200))
+        screen_width, _ = screen.get_size()
+        text_width, _ = self.planet_name.get_size()
+        img_width, _ = new_image.get_size()
+
+        if self.rect.x + self.text_offset_x + text_width + img_width > screen_width:
+            text_pos_x = self.rect.x - self.text_offset_x - text_width
+            text_pos_y = self.rect.y + self.text_offset_y
+        else:
+            text_pos_x = self.rect.x + self.text_offset_x + img_width
+            text_pos_y = self.rect.y + self.text_offset_y
+
+        screen.blit(new_image, (self.rect.x, self.rect.y))
+        screen.blit(self.planet_name, (text_pos_x, text_pos_y))
