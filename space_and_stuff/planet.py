e5ba77518354c7c3b2fe6102af31cb29a1c67f1a
Mirjana Babic <mirjana_babic@outlook.com>
2023-02-16 13:29:37 +0100

    Handle click on planets

--- space_and_stuff/planet.py
+++ space_and_stuff/planet.py
@@ -2,20 +2,26 @@ import pygame
 
 
 class Planet:
-    def __init__(self, x, y, image, name):
+    def __init__(self, x, y, image, name, on_click):
         self.image = image
         self.rect = self.image.get_rect()
-        self.rect.topleft = (x,y)
+        self.rect.topleft = (x, y)
+        self.on_click = on_click
 
         planet_name = name
         font = pygame.font.SysFont(None, 40)
-        text_color = (255,255,255)
+        text_color = (255, 255, 255)
 
         self.text_offset_x = -18
         self.text_offset_y = 50
-        self.planet_name= font.render(planet_name, True, text_color)
+        self.planet_name = font.render(planet_name, True, text_color)
         self.hover = False
 
+    def handle_event(self, event):
+        if event.type == pygame.MOUSEBUTTONDOWN:  # pylint: disable=no-member
+            pos = pygame.mouse.get_pos()
+            if self.rect.collidepoint(pos):
+                self.on_click()
 
     def is_hovering(self):
         mouse = pygame.mouse.get_pos()
@@ -23,7 +29,7 @@ class Planet:
         if self.rect.collidepoint(mouse):
             self.hover = True
         else:
-            self.hover= False
+            self.hover = False
 
         return self.hover
 
--- space_and_stuff/screens/game_screen.py
+++ space_and_stuff/screens/game_screen.py
@@ -1,5 +1,6 @@
 import os
 import pygame
+
 from screens import main_menu
 from planet import Planet
 from button import Button
@@ -27,31 +28,67 @@ class GameScreen:
             image=menu_button_img,
             on_click=self.on_menu_clicked)
 
-    def initialize_planet(self, x, y, image_name, name):
+    def initialize_planet(self, x, y, image_name, name, on_click):
         image = pygame.image.load(os.path.join("./pictures", image_name))
-        planet = Planet(x, y, image, name)
+        planet = Planet(x, y, image, name, on_click)
         self.planets.append(planet)
 
     def initialize_planets(self):
         self.planets = []
 
-        self.initialize_planet(x=680, y=380, image_name="andoria.png", name="Andoria")
-        self.initialize_planet(x=466, y=199, image_name="ardana.png", name="Ardana")
-        self.initialize_planet(x=1031, y=180, image_name="elball.png", name="Elball")
-        self.initialize_planet(x=160, y=90, image_name="nibiru.png", name="Nibiru")
-        self.initialize_planet(x=487, y=590, image_name="omegaIV.png", name="OmegaIV")
-        self.initialize_planet(x=1200, y=500, image_name="risa.png", name="Risa")
-        self.initialize_planet(x=750, y=50, image_name="vulcan.png", name="Vulcan")
-        self.initialize_planet(x=145, y=510, image_name="wolf359.png", name="Wolf359")
+        self.initialize_planet(x=680, y=380, image_name="andoria.png",
+                               name="Andoria", on_click=self.on_andoria_clicked)
+        self.initialize_planet(x=466, y=199, image_name="ardana.png",
+                               name="Ardana", on_click=self.on_ardana_clicked)
+        self.initialize_planet(x=1031, y=180, image_name="elball.png",
+                               name="Elball", on_click=self.on_elball_clicked)
+        self.initialize_planet(x=160, y=90, image_name="nibiru.png",
+                               name="Nibiru", on_click=self.on_nibiru_clicked)
+        self.initialize_planet(x=487, y=590, image_name="omegaIV.png",
+                               name="OmegaIV", on_click=self.on_omegaIV_clicked)
+        self.initialize_planet(
+            x=1200, y=500, image_name="risa.png", name="Risa", on_click=self.on_risa_clicked)
+        self.initialize_planet(x=750, y=50, image_name="vulcan.png",
+                               name="Vulcan", on_click=self.on_vulcan_clicked)
+        self.initialize_planet(x=145, y=510, image_name="wolf359.png",
+                               name="Wolf359", on_click=self.on_wolf359_clicked)
 
     def update(self):
         pass
 
     def on_menu_clicked(self):
-        self.game.set_screen(main_menu.MainMenu(self.game, self.screen, self.background))
+        self.game.set_screen(main_menu.MainMenu(
+            self.game, self.screen, self.background))
+
+    def on_andoria_clicked(self):
+        pass
+
+    def on_ardana_clicked(self):
+        pass
+
+    def on_elball_clicked(self):
+        pass
+
+    def on_nibiru_clicked(self):
+        pass
+
+    def on_omegaIV_clicked(self):
+        pass
+
+    def on_risa_clicked(self):
+        pass
+
+    def on_vulcan_clicked(self):
+        pass
+
+    def on_wolf359_clicked(self):
+        pass
 
     def handle_events(self, event):
         self.menu_button.handle_event(event)
+
+        for planet in self.planets:
+            planet.handle_event(event)
 
     def render(self):
         self.screen.blit(self.background, (0, 0))
